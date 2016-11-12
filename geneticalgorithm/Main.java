c3039bc6cbaab86ef8fd17365d814bb28f1279c8
Aaron <aaronjsunk@gmail.com>
2016-11-12 17:19:47 +0000

    write out to file

--- geneticalgorithm/Main.java
+++ geneticalgorithm/Main.java
@@ -1,5 +1,10 @@
 package geneticalgorithm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +17,11 @@ public class Main {
         for (int i = 0; i < iterations; i++) {
             currenIteration++;
             System.out.println("Iterations: " + currenIteration + " Fittest: " + population.getFittest().getFitness() + " Mean: " + population.getPopulationFitness());
+            try (PrintWriter out = new PrintWriter(new FileOutputStream(new File("results.csv"), true))) {
+                out.println(currenIteration + "," + (population.getFittest().getFitness()) + "," + population.getPopulationFitness() + ",");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
             population = GeneticAlgorithm.evolvePopulation(population);
         }
     }
